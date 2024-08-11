2d550c68081caf9459677dccff2af93a6991da6d
Debray-Deschodt <debraydeschodtpaul@gmail.com>
2024-08-11 04:49:26 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const whitelist = require('./whitelist')
 const analitics = require('./analitics')
+const path = require('path')
 
 router.use('/whitelist', whitelist)
 router.use('/analitics', analitics)
 router.use('/', (req, res) => {
-    res.status(404).end()
+    res.sendFile(path.join(__dirname, '../client-build/index.html'))
 })
 
 module.exports = router
